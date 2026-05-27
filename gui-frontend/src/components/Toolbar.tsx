fd123f9e3b992270c02d6c6f5852d652b3d83b5b
G3R4RD0 <gerardo.vasquez.p@gmail.com>
2026-05-27 12:04:14 +0200

    removed extra browser button

--- gui-frontend/src/components/Toolbar.tsx
+++ gui-frontend/src/components/Toolbar.tsx
@@ -1,6 +1,5 @@
 import { useUIStore } from "../stores/useUIStore";
 import { usePhotoStore } from "../stores/usePhotoStore";
-import { useFolderBrowse } from "../hooks/useFolderBrowse";
 
 import "./Toolbar.css";
 
@@ -9,14 +8,8 @@ export default function Toolbar() {
   const selectedCount = usePhotoStore((s) => s.selectedPaths.size);
   const totalCount = usePhotoStore((s) => s.photos.length);
   const status = useUIStore((s) => s.status);
-  const { browseFolder, isLoading } = useFolderBrowse();
   return (
     <div className="toolbar">
-      <div className="toolbar-left">
-        <button onClick={browseFolder} disabled={isLoading}>
-          📁 Browse
-        </button>
-      </div>
       <div className="toolbar-center">
         <span>
           {selectedCount} / {totalCount} selected
